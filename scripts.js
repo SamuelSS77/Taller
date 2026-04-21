6d95cb0a07596d52098c3b23ff89608faa7b757a
Samuel Santiago Silva <sssilva@unbosque.edu.co>
2026-04-20 22:19:00 -0500

    Update scripts.js
    AñadiConstantes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//variables
 let nombreCompleto = "Juan el Caballo Loco";
 let edad = 18;
 let tipoDeDocumento = "CC";
@@ -7,3 +8,13 @@ let salario = 0;
 let comisiones = 0;
 let totalHorasExtras = 0;
 let nivelRiesgo = "";
+
+//constantes
+const salarioMinimo = 1750905;
+const salarioMinimoIntegral = 22761765;
+const auxilioTransporte = 249095;
+const porcentajePension = 0.04;
+const porcentajeSalud = 0.04;
+const porcentajeFondoSolidaridad = 0.01;
+
+
