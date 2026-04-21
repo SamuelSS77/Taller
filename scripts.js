6f2cf1cffbc2acb970ffa68e566dfedd31f6df25
Samuel Santiago Silva <sssilva@unbosque.edu.co>
2026-04-20 22:15:15 -0500

    Update scripts.js
    AñadiVariables

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ let nombreCompleto = "Juan el Caballo Loco";
 let edad = 18;
 let tipoDeDocumento = "CC";
 let numeroDocumento = "1011099440";
+
+let salario = 0;
+let comisiones = 0;
+let totalHorasExtras = 0;
+let nivelRiesgo = "";
